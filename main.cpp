40c9eb45be18c9ccb6899dc0576f813c7d1c2c4d
Chris <chris@field.de>
2018-11-06 17:11:44 +0100

    Moved some things

--- main.cpp
+++ main.cpp
@@ -12,18 +12,18 @@ std::ostream& operator<<(std::ostream& s, const std::vector<T>& v) {
 	return s << ']';
 }
 
-std::ostringstream generateOutput(const std::vector<unsigned long> &a, long additionalCycles) {
+std::ostringstream generateOutput(const std::vector<unsigned long> &a) {
 	std::ostringstream output;
 
 	output << "asm volatile (\n";
-	for (int i = 0; i < a.size(); i++){
+	for (int i = 0; i < a.size() - 1; i++){
 		output << "\t\"\tldi  r" << i + 16 << ", " << a[i] << "\t\\n\"\n";
 	}
-	for (int i = 0; i < a.size(); i++){
+	for (int i = 0; i < a.size() - 1; i++){
 		output << "\t\"" << (i == 0 ? "L:" : "") << "\tdec  r" << a.size() + 15 - i << "\t\t\\n\"\n";
 		output << "\t\"\tbrne L\t\t\t\\n\"\n";
 	}
-	for (int i = 0; i < -additionalCycles; i++){
+	for (int i = 0; i < a[a.size() - 1]; i++){
 		output << "\t\"\tnop\t\t\t\t\\n\"\n";
 	}
 
@@ -47,21 +47,40 @@ int nestedLoopsRequired(unsigned long cycles) {
 	return loopsRequired;
 }
 
-bool verify(const std::vector<unsigned long> &a, long additionalCycles, unsigned long expected) {
-	unsigned long actual = 0;
-	for (int i = 0; i < a.size(); i++) {
-		actual += 3 + (a[a.size() - i - 1] - 1) * maxDelay(i);
+std::vector<unsigned long> calculateLoop(int n, unsigned long cycles) {
+	std::vector<unsigned long> a(static_cast<unsigned long>(n + 1));
+	long additionalCycles;
+
+	a[0] = (cycles + maxDelay(n - 1)) / maxDelay(n - 1);
+	additionalCycles = a[0] * maxDelay(n - 1) - cycles;
+
+	for (int i = 1; i < n; i++){
+		a[i] = 256 - additionalCycles / maxDelay(n - i - 1);
+		additionalCycles -= (256 - a[i]) * maxDelay(n - i - 1);
 	}
-	actual -= additionalCycles;
-	return actual == expected;
+	if (additionalCycles > 0) {
+		a[a.size() - 2] -= 1;
+		additionalCycles -= maxDelay(0);
+	}
+	a[a.size() - 1] = static_cast<unsigned long>(-additionalCycles);
+
+	return a;
+}
+
+unsigned long calculateDelay(const std::vector<unsigned long> &a) {
+	unsigned long actual = 0;
+	for (int i = 0; i < a.size() - 1; i++) {
+		actual += 3 + (a[a.size() - i - 2] - 1) * maxDelay(i);
+	}
+	actual += a[a.size() - 1];
+	return actual;
 }
 
 int main(int argc, char **argv){
 	if (argc < 3){
-		std::cout << "Usage: assembly-delayloop <TIME> <CLOCK>" << std::endl;
+		std::cerr << "Usage: assembly-delayloop <TIME> <CLOCK>" << std::endl;
 		exit(1);
 	}
-
 	std::map<std::string, int> prefixes{
 			{ "P", 15 },
 			{ "T", 12 },
@@ -93,25 +112,16 @@ int main(int argc, char **argv){
 //	unsigned long cycles = 480000000009;
 	int n = nestedLoopsRequired(cycles);
 	cycles -= n - 1;
-	long additionalCycles = 0;
 
-	std::vector<unsigned long> a(static_cast<unsigned long>(n));
-	a[0] = (cycles + maxDelay(n - 1))/ maxDelay(n - 1);
-	additionalCycles = a[0] * maxDelay(n - 1) - cycles;
+	std::vector<unsigned long> a = calculateLoop(n, cycles);
 
-	for (int i = 1; i < n; i++){
-		a[i] = 256 - additionalCycles / maxDelay(n - i - 1);
-		additionalCycles -= (256 - a[i]) * maxDelay(n - i - 1);
+	if (calculateDelay(a) != cycles + n - 1){
+		std::cerr << "Error" << std::endl;
+		std::cerr << "Expected: " << cycles + n - 1 << std::endl;
+		std::cerr << "Actual: " << calculateDelay(a) << std::endl;
 	}
-	if (additionalCycles > 0) {
-		a[a.size() - 1] -= 1;
-		additionalCycles -= maxDelay(0);
-	}
-	if (!verify(a, additionalCycles, cycles + n - 1)){
-		exit(2);
-	}
-//	std::cout << generateOutput(a, additionalCycles).str() << std::endl;
-	std::cout << a << " + " << -additionalCycles << std::endl;
+	std::cout << generateOutput(a).str() << std::endl;
+//	std::cout << a << std::endl;
 
 	return 0;
 }
